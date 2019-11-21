fb72821751c92f41c8776fc15eb86f061a737cb7
jkwourms <jkw64@case.edu>
2019-11-20 19:59:44 -0500

    Added a method to create a CSV of who the user is following.

--- scrape-n-scan.py
+++ scrape-n-scan.py
@@ -8,6 +8,13 @@ import json
 import csv
 from tqdm import tqdm
 
+def getFollowing(user):
+  whoUserFollows = requests.get(url = 'https://api.github.com/users/' + user + '/following').json()
+  followsList = []
+  for person in whoUserFollows:
+    followsList.append([person['login'], person['url']])
+  return followsList
+
 def getRepoLinks(user):
   data = requests.get(url = 'https://api.github.com/users/' + user + '/repos?per_page=100').json()
   print(len(data))
@@ -35,14 +42,31 @@ def main():
     rows = [['Project ID','Project Name','Project Link','Zip Link','HA Link','HA Detection Result','VT flagged']]
     count = 1
     repos = getRepoLinks(username)
+    
+    # Provide a visual indicator of scanning progress on the command line
     for repo in tqdm(repos):
       halink, vtresult = scan(repo[2])
       rows.append([str(count),repo[0],repo[1],repo[2],halink,'',vtresult])
       count += 1
+
+    # Write HA scans to CSV file in root folder
     with open(username+'.csv', 'w') as csvFile:
       writer = csv.writer(csvFile)
       writer.writerows(rows)
     csvFile.close()
+
+    #Get list of people Malicious users are following
+    following = getFollowing(username)
+    rows2 = [['Username', 'Profile Link']]
+    count2 = 1
+    for user in following:
+      rows2.append([str(count2),user[0],user[1]])
+      count2 += 1
+    with open(username+'FollowingList.csv', 'w') as csvFile:
+      writer = csv.writer(csvFile)
+      writer.writerows(rows2)
+    csvFile.close()
+
   else:
     print("Please input a valid Github username after the program name to run the scrapper")
 
