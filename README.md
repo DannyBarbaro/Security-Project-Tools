6c19f67c3fa428249e89df641b5bac1925b6421d
DannyBarbaro <dxb486@case.edu>
2019-11-20 20:46:38 -0500

    added crawling with a depth limit

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ Tools for the final project of EECS 349
 - pip install tqdm
 
 # How to run
-python3 scrape-n-scan.py DannyBarbaro
-- exports a DannyBarbaro.csv with the results
+python3 scrape-n-scan.py username
+- exports a username.csv with the results and a second file username_following.csv with the users they are following
+
+python3 scrape-n-scan.py username #
+- exports a bunch of csv files of data for the first user and crawls to a depth of # for their following users repos
--- scrape-n-scan.py
+++ scrape-n-scan.py
@@ -8,16 +8,27 @@ import json
 import csv
 from tqdm import tqdm
 
-def getFollowing(user):
-  whoUserFollows = requests.get(url = 'https://api.github.com/users/' + user + '/following').json()
-  followsList = []
-  for person in whoUserFollows:
-    followsList.append([person['login'], person['url']])
-  return followsList
+# Writes the csv for a user and returns a list of the people that they are following
+def scanUser(username): 
+  print(username)
+  rows = [['Project ID','Project Name','Project Link','Zip Link','HA Link','HA Detection Result','VT flagged']]
+  count = 1
+  repos = getRepoLinks(username)
+    
+  # Provide a visual indicator of scanning progress on the command line
+  for repo in tqdm(repos):
+    halink, vtresult = scan(repo[2])
+    rows.append([str(count),repo[0],repo[1],repo[2],halink,'',vtresult])
+    count += 1
+
+  # Write HA scans to CSV file in root folder
+  writeToCSV(username+'.csv', rows)
+
+  #Get list of people Malicious users are following
+  return getFollowing(username)
 
 def getRepoLinks(user):
   data = requests.get(url = 'https://api.github.com/users/' + user + '/repos?per_page=100').json()
-  print(len(data))
   repos = []
   for item in data:
     repos.append([item['name'], item['html_url'], item['html_url'] + '/archive/master.zip'])
@@ -36,37 +47,46 @@ def scan(url):
         vtResult = 'Yes'
   return haLink, vtResult
 
-def main():
-  if len(sys.argv) > 1:
-    username = sys.argv[1]
-    rows = [['Project ID','Project Name','Project Link','Zip Link','HA Link','HA Detection Result','VT flagged']]
-    count = 1
-    repos = getRepoLinks(username)
-    
-    # Provide a visual indicator of scanning progress on the command line
-    for repo in tqdm(repos):
-      halink, vtresult = scan(repo[2])
-      rows.append([str(count),repo[0],repo[1],repo[2],halink,'',vtresult])
-      count += 1
+def getFollowing(user):
+  whoUserFollows = requests.get(url = 'https://api.github.com/users/' + user + '/following').json()
+  followsList = []
+  for person in whoUserFollows:
+    followsList.append(person['login'])
+  return followsList
 
-    # Write HA scans to CSV file in root folder
-    with open(username+'.csv', 'w') as csvFile:
-      writer = csv.writer(csvFile)
-      writer.writerows(rows)
-    csvFile.close()
+def makeFollowingCSV(username, following):
+  rows = [['ID', 'Username', 'Profile Link']]
+  count = 1
+  for user in following:
+    rows.append([str(count),user,'https://github.com/'+user])
+    count += 1
+  writeToCSV(username+'_following.csv', rows)
 
-    #Get list of people Malicious users are following
-    following = getFollowing(username)
-    rows2 = [['Username', 'Profile Link']]
-    count2 = 1
+def writeToCSV(name, rows):
+  with open(name, 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerows(rows)
+  csvFile.close()
+
+def runCrawl(username, depth):
+  if depth > 0:
+    following = scanUser(username)
+    print('Number users following: ' +  str(len(following)))
     for user in following:
-      rows2.append([str(count2),user[0],user[1]])
-      count2 += 1
-    with open(username+'FollowingList.csv', 'w') as csvFile:
-      writer = csv.writer(csvFile)
-      writer.writerows(rows2)
-    csvFile.close()
+      runCrawl(user, depth-1)
 
+def main():
+  # Run one layer deep and return list of following
+  if len(sys.argv) == 2:
+    username = sys.argv[1]
+    # scan top 100 repos and get their followers
+    following = scanUser(username)
+    makeFollowingCSV(username, following)
+  # Run the defined number of layers deep and build all of the csv files
+  if len(sys.argv) == 3:
+    username = sys.argv[1]
+    depth = int(sys.argv[2])
+    runCrawl(username, depth)
   else:
     print("Please input a valid Github username after the program name to run the scrapper")
 
